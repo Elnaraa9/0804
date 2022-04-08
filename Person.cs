6d22c4893a5da84000139612d38d3a7749e951f7
Elnaraa9 <100670042+Elnaraa9@users.noreply.github.com>
2022-04-08 19:08:47 +0400

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08_04_2002.Models
+{
+    public class Person
+    {
+        internal static object result;
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public Person()
+        {
+        }
+    }
+}
